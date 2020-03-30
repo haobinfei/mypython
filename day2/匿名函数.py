17658b624ae3df76737e443c7b2555f0244ad96d
haobinfei <17853309409@163.com>
2020-03-31 00:31:29 +0800

    day2

---
+++ day2/匿名函数.py
@@ -0,0 +1,38 @@
+# 简化函数定义
+
+# 格式  lambda  参数A， 参数B ： 运算
+
+s = lambda a, b: a + b
+
+print(s(1, 2))
+
+s1 = lambda a, b: a * b
+
+t = s1(2, 5)
+print(t)
+
+
+def func(x, y, func):
+    print(x, y)
+    s = func(x, y)
+    print(s)
+
+
+func(2, 4, lambda x, y: x + y)
+
+# 作用
+list1 = [{"a": 10, "b": 20}, {"a": 15, "b": 24}, {"a": 1, "b": 0}]
+
+t = max(list1, key=lambda x: x["a"])
+
+print(t)
+
+list2 = (2, 4, 5, 6, 7, 8, 10)
+
+result = map(lambda x: x + 2, list2)
+
+print(list(result))
+
+result = map(lambda x: x if x == 2 else x + 2,list2)
+
+print(list(result))
